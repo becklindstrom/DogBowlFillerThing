ccc8e1ebdf7e3db58ce7cfacb63f5b663b9c118e
BonkyB <126713671+BonkyB@users.noreply.github.com>
2023-03-01 11:23:54 -0800

    Update ball_switch.ino

--- ball_switch.ino
+++ ball_switch.ino
@@ -1,24 +1,70 @@
-// Derived from example code provided by www.elegoo.com
 
-// Define pins
-const byte LED_PIN = LED_BUILTIN; // LED_BUILTIN = 13 on the Uno
-const byte SENSOR_PIN = 2;
-int state = 0;
+//Water Level Indicator Variables
+int level;
+int LED_pin = ;   //Digital
 
+
+//Filling Variables
+int bowlFill = True;
+int tankFill = True;
+int button_Pin = ;
+volatile unsigned long fillDuration = 0;
+
+
+//Ultrasonic Variables
+int trig_Pin = ;  //Digital
+int echo_Pin = ;  //Digital
+long sonicDuration;
+int sonicDistance;
+ 
 void setup() {
-  // Set up pins
-  pinMode(LED_PIN, OUTPUT);//initialize the ledPin as an output
-  pinMode(SENSOR_PIN, INPUT_PULLUP);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_Pin,OUTPUT);
+  pinMode(trig_Pin,OUTPUT);
+  pinMode(echo_Pin,INPUT);
+  pinMode(button_Pin,INPUT);
+  
 }
 
 void loop() {
-  state = digitalRead(SENSOR_PIN);
-  // Check the sensor state and act accordingly
-  if (state == HIGH) {
-    // Disconnected; turn the LED off
-    digitalWrite(LED_PIN, HIGH);
-  } else {
-    // Connected; turn the LED on
-    digitalWrite(LED_PIN, LOW);
+  
+  // put your main code here, to run repeatedly:
+  level = analogRead(A0);
+  Serial.println(level);
+  
+  if(level < 500){
+    digitalWrite(LED_Pin, HIGH); //Turn on LED to indicate low Bowl Water Level
+    bowlFill = False;
+
+    while(bowlFill == 'False' && tankFill == 'True'){
+      if(fillDuration < 1000){
+        //Run Motor
+      }
+      elseif(fillDuration >= 1000){
+        tankFill = False;
+      }
+    }
   }
+
+  if(button_Pin == HIGH){
+    tankFill = False;    
+  }
+
 }
+
+//Code Source: https://howtomechatronics.com/projects/arduino-radar-project/
+int calculateDistance(){ 
+  
+  digitalWrite(trig_Pin, LOW); 
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trig_Pin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trig_Pin, LOW);
+  sonic_Duration = pulseIn(echo_Pin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  sonicDistance= sonicDuration*0.034/2;
+  return sonicDistance;
+
+}
+
